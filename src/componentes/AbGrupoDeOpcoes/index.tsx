388cb422d52137ebf92abcd20207098c7e86e789
Fabio Leforte <fabio.leforte@gmail.com>
2023-06-26 22:40:06 -0300

    feat: criado tipo

--- src/componentes/AbGrupoDeOpcoes/index.tsx
+++ src/componentes/AbGrupoDeOpcoes/index.tsx
@@ -4,10 +4,11 @@ import { styled } from 'styled-components';
 const SectionEstilizada = styled.section<{ selecionado: boolean }>`
   width: 194px;
   height: 88px;
-  background: ${props =>
-    props.selecionado
+  background: ${function(props: any) {
+    return props.selecionado
       ? 'linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)'
-      : '#fff'};
+      : '#fff';
+  }};
   border: 1px solid;
   border-color: ${props => (props.selecionado ? '#002f52' : '#eb9b00')};
   border-radius: 8px;
@@ -49,11 +50,11 @@ export interface AbGrupoOpcoesProps {
   valorPadrao?: AbGrupoOpcao | null;
   onChange?: (opcao: AbGrupoOpcao) => void;
 }
-export const AbGrupoOpcoes = ({
+export const AbGrupoOpcoes: React.FC<AbGrupoOpcoesProps> = ({
   opcoes,
   onChange,
   valorPadrao,
-}: AbGrupoOpcoesProps) => {
+}) => {
   const [selecao, setSelecao] = useState<AbGrupoOpcao | null>(
     valorPadrao ?? null
   );
