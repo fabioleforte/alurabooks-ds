24acece8f289288f51c33cbed0abf653901014cd
Fabio Leforte <fabio.leforte@gmail.com>
2023-06-26 22:43:27 -0300

    feat: tipos any

--- src/componentes/AbGrupoDeOpcoes/index.tsx
+++ src/componentes/AbGrupoDeOpcoes/index.tsx
@@ -4,11 +4,10 @@ import { styled } from 'styled-components';
 const SectionEstilizada = styled.section<{ selecionado: boolean }>`
   width: 194px;
   height: 88px;
-  background: ${function(props: any) {
-    return props.selecionado
+  background: ${props =>
+    props.selecionado
       ? 'linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)'
-      : '#fff';
-  }};
+      : '#fff'};
   border: 1px solid;
   border-color: ${props => (props.selecionado ? '#002f52' : '#eb9b00')};
   border-radius: 8px;
@@ -20,18 +19,19 @@ const SectionEstilizada = styled.section<{ selecionado: boolean }>`
   font-family: sans-serif;
   cursor: pointer;
   header {
-    color: ${props => (props.selecionado ? '#fff' : '#eb9b00')};
+    color: ${(props: any) => (props.selecionado ? '#fff' : '#eb9b00')};
     font-size: 12px;
     font-weight: 400;
   }
   strong {
-    color: ${props => (props.selecionado ? '#fff' : '#eb9b00')};
+    color: ${(props: any) => (props.selecionado ? '#fff' : '#eb9b00')};
     font-size: 16px;
     font-weight: 700;
   }
 
   footer {
-    color: ${props => (props.selecionado ? '#fff' : 'rgba(0, 0, 0, 0.5)')};
+    color: ${(props: any) =>
+      props.selecionado ? '#fff' : 'rgba(0, 0, 0, 0.5)'};
 
     font-size: 12px;
     font-weight: 600;
